2bdfe11d51a485fef4a5d2319f1d5b674d81be8b
Gus Wezerek <gus@upstatement.com>
2014-01-16 15:31:44 -0500

    fixed font and image directories. file paths should now work regardless of root directory name (ie. chainsaw-feed vs feed-master)

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
-http_path = "/wp-content/plugins/chainsaw-feed/"
+http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "sass/img"
 javascripts_dir = "js"
-fonts_dir = "fonts"
+fonts_dir = "sass/fonts/entypo/"
 relative_assets = true
  
 line_comments = true
